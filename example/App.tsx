3b0ecba73ccfbe0cfd8f01ee3e6a8576bf297e47
ikcede <1250975+ikcede@users.noreply.github.com>
2024-08-26 18:35:08 -0400

    Add controlled bezier

--- example/App.tsx
+++ example/App.tsx
@@ -3,88 +3,36 @@ import './App.css';
 import { BezierEditor, CubicBezier } from '../src/components';
 
 function App() {
-  const [bezier, setBezier] = 
-      React.useState(new CubicBezier(0.25, 0.25, 0.75, 0.75));
-
-  const [x1, setX1] = React.useState('0.25');
-  const [y1, setY1] = React.useState('0.25');
-  const [x2, setX2] = React.useState('0.75');
-  const [y2, setY2] = React.useState('0.75');
+  const [bezier, setBezier] = React.useState(['0.25', '0.25', '0.75', '0.75']);
 
   const onBezierChange = (newBezier?: CubicBezier) => {
     if (newBezier !== undefined) {
-      setBezier(newBezier);
-      setX1(newBezier.x1.toString());
-      setY1(newBezier.y1.toString());
-      setX2(newBezier.x2.toString());
-      setY2(newBezier.y2.toString());
+      setBezier([...newBezier.toStringArray()]);
     }
   };
 
-  const changeX1 = (
+  const changeBezierValue = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    index: number
   ) => {
-    setX1(e.target.value);
-    let floatValue = parseFloat(e.target.value);
-    if (!Number.isNaN(floatValue)) {
-      bezier.x1 = floatValue;
-      setBezier(new CubicBezier(...bezier.toArray()));
-    }
-  }
-
-  const changeY1 = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setY1(e.target.value);
-    let floatValue = parseFloat(e.target.value);
-    if (!Number.isNaN(floatValue)) {
-      bezier.y1 = floatValue;
-      setBezier(new CubicBezier(...bezier.toArray()));
-    }
-  }
-
-  const changeX2 = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setX2(e.target.value);
-    let floatValue = parseFloat(e.target.value);
-    if (!Number.isNaN(floatValue)) {
-      bezier.x2 = floatValue;
-      setBezier(new CubicBezier(...bezier.toArray()));
-    }
-  }
-
-  const changeY2 = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setY2(e.target.value);
-    let floatValue = parseFloat(e.target.value);
-    if (!Number.isNaN(floatValue)) {
-      bezier.y2 = floatValue;
-      setBezier(new CubicBezier(...bezier.toArray()));
-    }
+    bezier[index] = e.target.value;
+    setBezier([...bezier]);
   }
 
   return (
     <div className='App'>
       <div className='component-wrapper'>
         <BezierEditor 
-            bezier={bezier}
+            bezier={CubicBezier.fromStringArray(bezier) ?? undefined}
             onChange={onBezierChange}></BezierEditor>
       </div>
       <div>
-        <input type='text'
-                value={x1}
-                onChange={changeX1}></input>
-        <input type='text'
-                value={y1}
-                onChange={changeY1}></input>
-        <input type='text'
-                value={x2}
-                onChange={changeX2}></input>
-        <input type='text'
-                value={y2}
-                onChange={changeY2}></input>
+        {bezier.map((val, index) => 
+          <input type='text'
+                 value={val}
+                 key={index}
+                 onChange={(e) => changeBezierValue(e, index)}></input>
+        )}
       </div>
     </div>
   );
--- src/util/CubicBezier.ts
+++ src/util/CubicBezier.ts
@@ -9,6 +9,18 @@ export default class CubicBezier {
     this.setValues(x1, y1, x2, y2);
   }
 
+  /// Helper to build a CubicBezier from an array
+  static fromArray(arr: number[]): CubicBezier {
+    return new CubicBezier(arr[0], arr[1], arr[2], arr[3]);
+  }
+
+  /// Helper to build a CubicBezier from an array of strings
+  static fromStringArray(arr: string[]): CubicBezier | null {
+    let nums = arr.map(parseFloat);
+    if (Number.isNaN(nums[0] + nums[1] + nums[2] + nums[3])) return null;
+    return CubicBezier.fromArray(nums);
+  }
+
   /// Set values, ensuring that x is between [0, 1]
   setValues(x1: number, y1: number, x2: number, y2: number): void {
     this.x1 = x1 >= 0 ? (x1 <= 1 ? x1 : 1) : 0;
@@ -32,5 +44,15 @@ export default class CubicBezier {
   /// Converts CubicBezier into an array
   toArray(): [number, number, number, number] {
     return [this.x1, this.y1, this.x2, this.y2];
+  }
+
+  /// Converts CubicBezier to a string array
+  toStringArray(): [string, string, string, string] {
+    return [
+      this.x1.toString(),
+      this.y1.toString(),
+      this.x2.toString(),
+      this.y2.toString()
+    ];
   }
 }
