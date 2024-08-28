7a63fa007f23aa405373c33a0fe90289a621145d
ikcede <1250975+ikcede@users.noreply.github.com>
2024-08-28 00:24:53 -0400

    Update example

--- example/App.tsx
+++ example/App.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 import { BezierEditor, CubicBezier } from '../src/components';
 
+const textTimingMap: {
+  [key: string]: [string, string, string, string]
+} = {
+  'linear': ['0.25', '0.25', '0.75', '0.75'],
+  'ease': ['0.25', '0.1', '0.25', '1'],
+  'ease-in': ['0.42', '0', '1', '1'],
+  'ease-out': ['0', '0', '0.58', '1'],
+  'ease-in-out': ['0.42', '0', '0.58', '1']
+};
+
 function App() {
   const [bezier, setBezier] = React.useState(['0.25', '0.25', '0.75', '0.75']);
 
@@ -19,6 +29,10 @@ function App() {
     setBezier([...bezier]);
   }
 
+  const setType = (key: string) => {
+    setBezier(textTimingMap[key]);
+  }
+
   return (
     <div className='App'>
       <div className='component-wrapper'>
@@ -33,6 +47,11 @@ function App() {
                  key={index}
                  onChange={(e) => changeBezierValue(e, index)}></input>
         )}
+      </div>
+      <div style={{marginTop: '16px'}}>
+        {Object.keys(textTimingMap).map(key => (
+          <button key={key} onClick={() => setType(key)}>{key}</button>
+        ))}
       </div>
     </div>
   );
